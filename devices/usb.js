d5ab76bfd75c3bb2c4e944385aced95805304a1f
dinesh <r.dinesh@directfn.com>
2019-08-13 01:18:36 +0530

    socket

--- devices/usb.js
+++ devices/usb.js
@@ -17,26 +17,33 @@ function getUsbList(next) {
 
 function connectDevice(req, next) {
     const port = new SerialPort(req.body.comName, {autoOpen: false});
-    port.open(function (err) {
-        if (err) {
-            return console.log('Error opening port: ', err.message)
-        }
-    });
-    port.on('readable', function () {
-        port.read();
-    });
-    port.on('data', function (data) {
-        // for (const value of data.values()) {
-        //     console.log(value);
-        // }
-        readData(data,function (p) {
-            socket.EmitUpdateAllUsers({data:  p})
-        });
+    const parser = port.pipe(new delimiter({ delimiter: '\n'}));
 
+
+
+    parser.on('data', (data) => {
+       console.log('dataReady', data);
     });
-    this.port.parser.on('data', function (data) {
-        console.log('Data:', data, data.toString('utf8'));
-    });
+//     port.open(function (err) {
+//         if (err) {
+//             return console.log('Error opening port: ', err.message)
+//         }
+//     });
+//     port.on('readable', function () {
+//         port.read();
+//     });
+//     port.on('data', function (data) {
+//         // for (const value of data.values()) {
+//         //     console.log(value);
+//         // }
+//         readData(data,function (p) {
+//             socket.EmitUpdateAllUsers({data:  p})
+//         });
+//
+//     });
+//     this.port.parser.on('data', function (data) {
+//         console.log('Data:', data, data.toString('utf8'));
+//     });
 }
 ;
 function readData(buffer,next) {
