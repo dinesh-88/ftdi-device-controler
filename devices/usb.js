3d86cb4ec57c07c79f28d3ad3f92f53e140b2d5a
dinesh <r.dinesh@directfn.com>
2019-08-12 11:10:13 +0530

    socket

--- devices/usb.js
+++ devices/usb.js
@@ -16,7 +16,7 @@ function getUsbList(next) {
 }
 
 function connectDevice(req, next) {
-    const port = new SerialPort(req.body.comName, { autoOpen: false });
+    const port = new SerialPort(req.body.comName, {autoOpen: false});
     port.open(function (err) {
         if (err) {
             return console.log('Error opening port: ', err.message)
@@ -29,11 +29,37 @@ function connectDevice(req, next) {
         for (const value of data.values()) {
             console.log(value);
         }
-        socket.EmitUpdateAllUsers({data:data})
+        readData(data);
+        socket.EmitUpdateAllUsers({data:  data.toString('utf8')})
     });
     this.port.parser.on('data', function (data) {
-        console.log('Data:', data,data.toString('utf8'));
+        console.log('Data:', data, data.toString('utf8'));
     });
+}
+
+function readData(buffer) {
+    console.log(buffer.toString('ascii'))
+    // console.log(buffer.toString('ascii'))
+    const len = -1;
+    let packetCount = 0;
+    let sampleRate = 0;
+    let rawBuffer = [[]];
+    // if (len > 0) {
+    //     for (let i = 0; i < len; i++) {
+    //         if ((buffer[i]) === 90 && i + 15 <= len) {
+    //
+    //             if ((buffer[i + 15]) >= 0 && ((buffer[i + 15]) <= 15)) {
+    //                 packetCount++;
+    //                 if (packetCount >= sampleRate) {
+    //                     packetCount = 0;
+    //                     console.log("Rate : " + sampleRate);
+    //                     console.log("Time : "+ process.hrtime()[1]);
+    //                 }
+    //             }
+    //
+    //         }
+    //     }
+    // }
 }
 
 module.exports = {
